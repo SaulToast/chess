243ab9e273fe24ed81e6c240f103c7460c0933c1
Saul Hanson <3pathstoperfection@gmail.com>
2025-01-10 12:27:42 -0700

    The resetBoard method in ChessBoard now passes it's test. Overrode the equals method for ChessPiece

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -21,7 +24,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        board[position.getRow()][position.getColumn()] = piece;
+        board[8-position.getRow()][position.getColumn()-1] = piece;
     }
 
     /**
@@ -32,7 +35,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        return board[position.getRow()][position.getColumn()];
+        return board[8-position.getRow()][position.getColumn()-1];
     }
 
     /**
@@ -40,6 +43,88 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
+        // needs to have pieces, not just be an empty board
         board = new ChessPiece[8][8];
+
+        // all pawns
+        resetPawns(ChessGame.TeamColor.BLACK);
+        resetPawns(ChessGame.TeamColor.WHITE);
+
+        // rooks
+        resetPairs(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK, 1, 8);
+        resetPairs(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK, 1, 8);
+
+        // knights
+        resetPairs(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT, 2, 7);
+        resetPairs(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT, 2, 7);
+
+        // bishops
+        resetPairs(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP, 3, 6);
+        resetPairs(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP, 3, 6);
+
+        // queens
+        var blackQueen = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN);
+        addPiece(new ChessPosition(8, 4), blackQueen);
+        var whiteQueen = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.QUEEN);
+        addPiece(new ChessPosition(1, 4), whiteQueen);
+
+        // kings
+        var blackKing = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KING);
+        addPiece(new ChessPosition(8, 5), blackKing);
+        var whiteKing = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING);
+        addPiece(new ChessPosition(1, 5), whiteKing);
+
+
+    }
+
+    private int getDefaultRowFromColor(ChessGame.TeamColor color) {
+        int row;
+        if (color == ChessGame.TeamColor.BLACK){
+            row = 8;
+        } else { row = 1; }
+        return row;
+    }
+
+    private void resetPairs(ChessGame.TeamColor color, ChessPiece.PieceType pieceType, int start, int end) {
+        int row = getDefaultRowFromColor(color);
+        for (int i = start; i <= end; i+=(end-start)) {
+            var piece = new ChessPiece(color, pieceType);
+            addPiece(new ChessPosition(row, i), piece);
+        }
+    }
+
+    private void resetPawns(ChessGame.TeamColor color){
+        int row;
+        if (color == ChessGame.TeamColor.BLACK){
+            row = 7;
+        } else { row = 2; }
+
+        for (int i = 1; i < 9; i++) {
+            var pawn = new ChessPiece(color, ChessPiece.PieceType.PAWN);
+            addPiece(new ChessPosition(row, i), pawn);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessBoard that = (ChessBoard) o;
+
+        for (int i = 1; i <= 8; i++) {
+            for (int j = 1; j <= 8; j++) {
+                var pos = new ChessPosition(i, j);
+                if (!Objects.equals(getPiece(pos), that.getPiece(pos))){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(board);
     }
 }
