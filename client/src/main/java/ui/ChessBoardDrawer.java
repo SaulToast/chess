ff1dd788d78d8d8eb12eb003836ab4cfe8ac2e6d
Saul Hanson <3pathstoperfection@gmail.com>
2025-03-25 20:07:52 -0600

    Draws an empty chessboard correctly based on perspective. Just needs the pieces to be drawn

--- client/src/main/java/ui/ChessBoardDrawer.java
+++ client/src/main/java/ui/ChessBoardDrawer.java
@@ -13,6 +13,8 @@ public class ChessBoardDrawer {
 
     private final PrintStream out;
     private final TeamColor perspective;
+    private final String[] yLables = {"1", "2", "3", "4", "5", "6", "7", "8"};
+    private final String[] xLabels = {"a", "b", "c", "d", "e", "f", "g", "h"};
     
     public ChessBoardDrawer(TeamColor color) {
         out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
@@ -24,13 +26,13 @@ public class ChessBoardDrawer {
 
         drawRowBorder();
         drawSquares();
+        drawRowBorder();
 
         out.print(SET_BG_COLOR_BLACK);
         out.print(SET_TEXT_COLOR_WHITE);
     }
 
     private void drawRowBorder() {
-        String[] xLabels = {"a", "b", "c", "d", "e", "f", "g", "h"};
 
         int direction = 1;
         int index = 0;
@@ -43,17 +45,46 @@ public class ChessBoardDrawer {
         out.print(SET_BG_COLOR_LIGHT_GREY);
         out.print(SET_TEXT_COLOR_LIGHT_GREY);
 
-        for (int col = 0; col != BOARD_SIZE; ++col) {
+        for (int col = 0; col != BOARD_SIZE; col++) {
             if (col > 0 && col < 9) {
-                out.print("  " + xLabels[index] + "  ");
+                out.print(" " + xLabels[index] + " ");
                 index += direction;
             } else {
                 out.print(EMPTY);
             }
         }
+
+        out.println();
     }
 
     private void drawSquares() {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (j == 0 || j == 9) {
+                    drawColBorderSquare(i);
+                    continue;
+                }
+                if ((i % 2 == 0 && j % 2 == 1) || (i % 2 == 1 && j % 2 == 0)) {
+                    out.print(SET_BG_COLOR_WHITE);
+                    out.print(SET_TEXT_COLOR_WHITE);
+                    out.print(EMPTY);
+                } else {
+                    out.print(SET_BG_COLOR_BLACK);
+                    out.print(SET_TEXT_COLOR_BLACK);
+                    out.print(EMPTY);
+                }
+            }
+            out.println();
+        }
+    }
 
+    private void drawColBorderSquare(int row) {
+        int index = row;
+        if (perspective != TeamColor.BLACK) {
+            index = 7-row;
+        }
+        out.print(SET_BG_COLOR_LIGHT_GREY);
+        out.print(SET_TEXT_COLOR_LIGHT_GREY);
+        out.print(" " + yLables[index] + " ");
     }
 }
