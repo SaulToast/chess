5932e36dcddb5bb58624270f082f3917a8eba934
Saul Hanson <3pathstoperfection@gmail.com>
2025-04-02 15:38:44 -0600

    Added files for the server side websocket handler

---
+++ client/src/main/java/facade/WebSocketFacade.java
@@ -0,0 +1,11 @@
+package facade;
+
+import javax.websocket.*;
+
+public class WebSocketFacade extends Endpoint {
+
+    @Override
+    public void onOpen(Session session, EndpointConfig config) {
+    }
+
+}
---
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -0,0 +1,12 @@
+package server.websocket;
+
+import exceptions.ResponseException;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+
+@WebSocket
+public class WebSocketHandler {
+    
+
+}
